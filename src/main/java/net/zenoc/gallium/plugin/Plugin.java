dd920945ea2396c3cfe0dc5d29cc3f916a2e23a1
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-06-23 22:32:10 +1200

    Store PluginMeta in the Plugin's instance

--- src/main/java/net/zenoc/gallium/plugin/Plugin.java
+++ src/main/java/net/zenoc/gallium/plugin/Plugin.java
@@ -1,7 +1,23 @@
 package net.zenoc.gallium.plugin;
 
-import net.zenoc.gallium.Gallium;
-
 public class Plugin {
+    private PluginMeta meta;
 
+    /**
+     * Sets the plugin's {@link PluginMeta}
+     * @param meta The {@link PluginMeta}
+     */
+    public void setMeta(PluginMeta meta) {
+        if (this.meta == null) {
+            this.meta = meta;
+        }
+    }
+
+    /**
+     * Get the plugin's {@link PluginMeta}
+     * @return The plugin's {@link PluginMeta}
+     */
+    public PluginMeta getMeta() {
+        return this.meta;
+    }
 }
