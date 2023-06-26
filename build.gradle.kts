28b67635d4e2bb16ba312b13ec17aea37e524f9f
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-06-26 16:08:43 +1200

    Use Kotlin DSL for Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+}
+
+group = "net.zenoc.gallium"
+version = "1.1.0-beta.3"
+
+repositories {
+    mavenCentral()
+    // mavenLocal()
+    maven("https://libraries.minecraft.net")
+    maven("https://repo.zenoc.net/repository")
+}
+
+dependencies {
+    compileOnly("com.mojang:brigadier:1.0.18")
+    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
+    implementation("net.kyori:adventure-text-serializer-gson:4.2.0")
+    implementation("net.kyori:event-method-asm:3.0.0")
+    implementation("org.json:json:20230618")
+}
