cdebac0c78666685c1f801e3253ff90d0df78dc3
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-07-01 13:51:44 +1200

    Bump the version in /gallium

--- src/main/java/net/zenoc/gallium/internal/plugin/commands/GalliumCommand.java
+++ src/main/java/net/zenoc/gallium/internal/plugin/commands/GalliumCommand.java
@@ -9,7 +9,7 @@ import net.zenoc.gallium.commandsys.CommandContext;
 public class GalliumCommand {
     @Command(aliases = {"gallium"}, description = "Information about Gallium")
     public void galliumCommand(CommandContext ctx) {
-        ctx.getCaller().sendMessage(ChatMessage.from(Colors.GREEN + "--- Gallium " + Colors.WHITE + "-" + Colors.GREEN + " Version 1.0.0 ---"));
+        ctx.getCaller().sendMessage(ChatMessage.from(Colors.GREEN + "--- Gallium " + Colors.WHITE + "-" + Colors.GREEN + " Version 1.1.0-beta.3 ---"));
         ctx.getCaller().sendMessage(ChatMessage.from(Colors.GREEN + "Developers: " + Colors.WHITE + "SlimeDiamond, TheKodeToad"));
 //        ctx.getCaller().sendMessage(ChatMessage.from(Colors.GREEN + "Contributors: " + Colors.WHITE + "Nobody! Maybe be the first?"));
         ctx.getCaller().sendMessage(ChatMessage.from(Colors.GREEN + "API version: " + Colors.WHITE + Gallium.getVersion()));
